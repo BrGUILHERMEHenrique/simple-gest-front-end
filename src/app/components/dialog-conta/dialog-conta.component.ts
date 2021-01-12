59ef77a7c6ce1061f6290d00c8d6a7460f9b0171
BrGUILHERMEHenrique <guilhermehenriquersoua17@gmail.com>
2021-01-12 12:08:45 -0300

    Criando dialog para criação de conta

--- src/app/components/dialog-conta/dialog-conta.component.ts
+++ src/app/components/dialog-conta/dialog-conta.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import api from 'src/services/api';
 
 @Component({
   selector: 'app-dialog-conta',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DialogContaComponent implements OnInit {
 
-  constructor() { }
 
+  listaTiposGastos:Array<String> = [];
+  constructor(
+    ) { }
+
+    async pegarTipoGastos(): Promise<void> {
+      try {
+        let response = await api.get('/tipo');
+        this.listaTiposGastos = response.data;
+        console.log('Tipo gatos lista: ', this.listaTiposGastos);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    async cadastrarNovaConta(): Promise<void> {
+      try {
+        let response = await api.post('/contas/criar', )
+      } catch (error) {
+        
+      }
+    }
   ngOnInit(): void {
+    this.pegarTipoGastos();
   }
 
 }
